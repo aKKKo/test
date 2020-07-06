af5903c5f329780f098d43a1d44d02b1ac66d948
Ivan <AkinKonstantin@yandex.ru>
2020-07-06 20:56:59 +0300

    Комитим домашку

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 
 // var number = 5;
 // var string = "Hello!";
@@ -44,11 +44,61 @@
 
 // console.log(typeof(!!null));
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
 
-let isChecked = false,
-    isClose = true;
+// let isChecked = false,
+//     isClose = true;
 
-console.log(isChecked || isClose);
+// console.log(isChecked || isClose);
+
+
+if (num < 49) {
+    console.log ("Не верно")
+} else if (num > 100) {
+    console.log ("МногО!");
+} else {
+    console.log ("Верно");
+};
+
+(num == 50) ? console.log ("Верно") : console.log ("Не верно");
+
+switch (num) {
+    case num < 49:
+        console.log ("Не верно");
+        break;
+    case num > 100:
+        console.log ("Много");
+        break;
+    case num > 80:
+        console.log ("Все еще много");
+        break;
+    case 50:
+        console.log ("Верно");
+        break;
+    default:
+        console.log ("сосамба");
+        break;
+}
+
+let num = 50;
+
+// while (num < 55) {
+//     console.log (num);
+//     num++;
+// }
+
+do {
+    console.log (num);
+    num++;
+}
+while (num < 55)
+
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6){
+        continue
+    }
+    console.log (i)
+}
